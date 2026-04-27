24c51100ce042b77689e302a54290a7cddf2311e
Rijushree007 <60056104+Rijushree123@users.noreply.github.com>
2026-04-28 03:03:57 +0530

    add frontend backend login signup logout

--- backend/qualityops-api/src/main/java/com/qualityops/portal/config/SecurityConfig.java
+++ backend/qualityops-api/src/main/java/com/qualityops/portal/config/SecurityConfig.java
@@ -5,13 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.qualityops.portal.security.JwtAuthFilter;
 
 @Configuration
 public class SecurityConfig {
+	private final JwtAuthFilter jwtAuthFilter;
+
+	public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+		this.jwtAuthFilter = jwtAuthFilter;
+	}
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
 		http.cors(cors -> {
 		}).csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
@@ -19,7 +26,7 @@ public class SecurityConfig {
 						.requestMatchers(HttpMethod.GET, "/api/health").permitAll()
 						.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest().authenticated())
 				.formLogin(form -> form.disable()).httpBasic(httpBasic -> httpBasic.disable());
-
+		http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
 }
--- backend/qualityops-api/src/main/java/com/qualityops/portal/security/JwtUtil.java
+++ backend/qualityops-api/src/main/java/com/qualityops/portal/security/JwtUtil.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
@@ -17,5 +18,22 @@ public class JwtUtil {
 		return Jwts.builder().subject(email).issuedAt(new Date())
 				.expiration(new Date(System.currentTimeMillis() + 86400000))
 				.signWith(Keys.hmacShaKeyFor(SECRET.getBytes())).compact();
+	}
+
+	public String extractEmail(String token) {
+
+		Claims claims = Jwts.parser().verifyWith(Keys.hmacShaKeyFor(SECRET.getBytes())).build().parseSignedClaims(token)
+				.getPayload();
+
+		return claims.getSubject();
+	}
+
+	public boolean isValid(String token) {
+		try {
+			extractEmail(token);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 }
